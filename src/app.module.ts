1c036de3c0f411a18f55776967b4f6ea083ec958
Douvry <valentin.jabba@gmail.com>
2022-01-05 09:30:17 +0400

    use .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,25 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RolesModule } from './roles/roles.module';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
-  UsersModule,
-  RolesModule,
-  MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      load: [configuration],
+    }),
+    MongooseModule.forRoot(`mongodb://${configuration().database.mongo.host}:` +
+      `${configuration().database.mongo.port}/${configuration().database.mongo.database}`
+    ,{ serverSelectionTimeoutMS: 5000 }),
+    /* MongooseModule.forRoot(`mongodb://${configuration().database.mongo.username}:` +
+      `${configuration().database.mongo.password}@${configuration().database.mongo.host}:` +
+      `${configuration().database.mongo.port}/${configuration().database.mongo.database}` + 
+      `?&authSource=admin`, { serverSelectionTimeoutMS: 5000, useFindAndModify: false }), */
+    UsersModule,
+    RolesModule,
   ],
   
   controllers: [AppController],
