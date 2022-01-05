e7ba8269000c5bcaaa739a462222453ba70164d4
Dylan Domen <dylan.domen@gmail.com>
2022-01-05 09:07:25 +0100

    add roles + connection to mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RolesModule } from './roles/roles.module';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+  UsersModule,
+  RolesModule,
+  MongooseModule.forRoot('mongodb://localhost/nest'),
+  ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/roles/roles.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { RolesController } from './roles.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RoleSchema } from 'src/schemas/role.schema';
+import { Role } from './entities/role.entity';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Role.name, schema: RoleSchema }])],
+  controllers: [RolesController],
+  providers: [RolesService]
+})
+export class RolesModule {}
